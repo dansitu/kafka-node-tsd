51ea8cebe32b87a8eb58478a28318589770d5193
Daniel Imrie-Situnayake <dan.situnayake@hackreactor.com>
2015-03-07 14:42:32 -0800

    Making progress

---
+++ kafka-node-test.js
@@ -0,0 +1,74 @@
+/// <reference path="typings/kafka-node.d.ts" />
+var kafka = require('kafka-node');
+var basicClient = new kafka.Client('localhost:2181/', 'sendMessage');
+var optionsClient = new kafka.Client('localhost:2181/', 'sendMessage', {
+    sessionTimeout: 30000,
+    spinDelay: 1000,
+    retries: 0
+});
+optionsClient.close();
+optionsClient.close(function () {
+});
+var producer = new kafka.Producer(basicClient);
+producer.on('ready', function () {
+    var messages = [{
+        topic: 'topicName',
+        messages: ['message body'],
+        partition: 0,
+        attributes: 2
+    }, {
+        topic: 'topicName',
+        messages: ['message body'],
+        partition: 0
+    }, {
+        topic: 'topicName',
+        messags: ['message body'],
+        attributes: 0
+    }, {
+        topic: 'topicName',
+        messages: ['message body']
+    }, {
+        topic: 'topicName',
+        messages: [new kafka.KeyedMessage('key', 'message')]
+    }];
+    producer.send(messages, function (err) {
+    });
+    producer.send(messages, function (err, data) {
+    });
+    producer.createTopics(['t'], true, function (err, data) {
+    });
+    producer.createTopics(['t'], false, function (err, data) {
+    });
+    // producer.createTopics(['t'], function (err: Error, data: Object) {}); // Omitting middle argument is not possible in TS
+});
+var highLevelProducer = new kafka.HighLevelProducer(basicClient);
+highLevelProducer.on('ready', function () {
+    var messages = [{
+        topic: 'topicName',
+        messages: ['message body'],
+        attributes: 2
+    }, {
+        topic: 'topicName',
+        messages: ['message body'],
+        partition: 0
+    }, {
+        topic: 'topicName',
+        messags: ['message body'],
+        attributes: 0
+    }, {
+        topic: 'topicName',
+        messages: ['message body']
+    }, {
+        topic: 'topicName',
+        messages: [new kafka.KeyedMessage('key', 'message')]
+    }];
+    producer.send(messages, function (err) {
+    });
+    producer.send(messages, function (err, data) {
+    });
+    producer.createTopics(['t'], true, function (err, data) {
+    });
+    producer.createTopics(['t'], false, function (err, data) {
+    });
+    // producer.createTopics(['t'], function (err: Error, data: Object) {}); // Omitting middle argument is not possible in TS
+});
--- typings/kafka-node.d.ts
+++ typings/kafka-node.d.ts
@@ -1,35 +1,67 @@
+// Type definitions for kafka-node 0.2.22
+// Project: https://github.com/SOHU-Co/kafka-node/
+// Definitions by: Daniel Imrie-Situnayake <https://github.com/dansitu/>
+// Definitions: https://github.com/dansitu/kafka-node-tsd
+
 declare module 'kafka-node' {
 
-  export interface Client {
+    // Client
+    export interface Client {
+        close(callback?: Function): void;
+    }
 
-  }
+    export interface ZKOptions {
+        sessionTimeout?: number;
+        spinDelay?: number;
+        retries?: number;
+    }
 
-  interface ClientFactory {
-    new(connectionString: string, clientId: string, options?: Object): Client;
-  }
+    interface ClientFactory {
+        new(connectionString: string, clientId: string, options?: ZKOptions): Client;
+    }
 
-  export interface HighLevelProducer {
-    on(eventName: string, cb: () => void): void;
-    send(payloads: Array<IProduceRequest>, cb: (error: any, data: any) => void): void;
-  }
+    // Producer
+    export interface Producer {
+        on(eventName: string, cb: () => void): void;
+        send(payloads: any, cb: (error: Error, data: Object) => void): void;
+        createTopics(topics: Array<string>, async: boolean, cb?: (error: Error, data: Object) => void): void;
+    }
 
-  export interface IProduceRequest {
-    topic: string;
-    messages: any;
-  }
+    interface ProducerFactory {
+        new(client: Client): Producer;
+    }
 
-  export interface SingleMessageProduceRequest {
-    messages: string;
-  }
-  export interface MultiMessageProduceRequest {
-    messages: Array<string>;
-  }
+    // HighLevelProducer
+    export interface HighLevelProducer {
+        on(eventName: string, cb: () => void): void;
+        send(payloads: Array<IProduceRequest>, cb: (error: Error, data: Object) => void): void;
+        createTopics(topics: Array<string>, async: boolean, cb?: (error: Error, data: Object) => void): void;
+    }
 
-  interface HighLevelProducerFactory {
-    new(client: Client): HighLevelProducer;
-  }
+    interface HighLevelProducerFactory {
+        new(client: Client): HighLevelProducer;
+    }
 
-  var Client: ClientFactory;
-  var HighLevelProducer: HighLevelProducerFactory;
+    // KeyedMessage
+    export interface KeyedMessage {
+
+    }
+
+    interface KeyedMessageFactory {
+        new(key: string, message: string): KeyedMessage;
+    }
+
+    // ProduceRequests
+    interface IProduceRequest {
+        topic: string;
+        partition?: number;
+        attributes?: number;
+    }
+
+    // API
+    var Client: ClientFactory;
+    var KeyedMessage: KeyedMessageFactory;
+    var Producer: ProducerFactory;
+    var HighLevelProducer: HighLevelProducerFactory;
 
 }
